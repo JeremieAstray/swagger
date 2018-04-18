94638034e93a12b8286a62ed1055455bf3015570
jeremieastrayye <jeremieastray@gmail.com>
2018-04-18 14:21:54 +0800

    init

---
+++ src/main/java/io/github/jeremieastray/swagger/Main.java
@@ -0,0 +1,11 @@
+package io.github.jeremieastray.swagger;
+
+/**
+ * @author guanhong 2018/4/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        var temp = "temp";
+        System.out.println(temp);
+    }
+}
